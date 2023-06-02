342506cbaf03a1393d393f65e3a79b355cbb67c5
DianaRatnikova <Diana_r@bk.ru>
2023-06-02 23:28:12 +0300

    Изменила тесты с учётом размера доски в параметре функций

--- tests/test_check_main_diagonal_match.py
+++ tests/test_check_main_diagonal_match.py
@@ -4,36 +4,39 @@ import pytest
 
 
 @pytest.mark.parametrize(
-  "game_board_list, expected_result",
+  "game_board_list, width_of_board, expected_result",
   [
-    ([[NAME_EMPTY, NAME_EMPTY], [NAME_EMPTY, NAME_EMPTY]], False),
+    ([[NAME_EMPTY, NAME_EMPTY], [NAME_EMPTY, NAME_EMPTY]], 3, False),
     ([[0, NAME_EMPTY, NAME_EMPTY], 
     [NAME_EMPTY, 0, NAME_EMPTY], 
     [NAME_EMPTY, NAME_EMPTY, 0]], 
+    3,
     True),
     ([['A', NAME_EMPTY, NAME_EMPTY], 
     [NAME_EMPTY, 'A', NAME_EMPTY], 
     [NAME_EMPTY, NAME_EMPTY, 'A']], 
+    3,
     True),
     ([['A', NAME_EMPTY, NAME_EMPTY], 
     [NAME_EMPTY, 'B', NAME_EMPTY], 
     [NAME_EMPTY, NAME_EMPTY, 'A']], 
+    3,
     False),
   ]      
 )
-def test__check_main_diagonal_match__is_valid(game_board_list, expected_result):
-    assert check_main_diagonal_match(game_board_list) is expected_result
+def test__check_main_diagonal_match__is_valid(game_board_list, width_of_board, expected_result):
+    assert check_main_diagonal_match(game_board_list, width_of_board) is expected_result
 
 
 @pytest.mark.parametrize(
-  "game_board_list, expected_error",
+  "game_board_list, width_of_board, expected_error",
   [
-    ([], IndexError),
-    ([NAME_EMPTY, NAME_EMPTY, NAME_EMPTY], TypeError),
+    ([], 3, IndexError),
+    ([NAME_EMPTY, NAME_EMPTY, NAME_EMPTY], 3, TypeError),
   ]      
 )
-def test__check_main_diagonal_match__errors(game_board_list, expected_error):
+def test__check_main_diagonal_match__errors(game_board_list, width_of_board, expected_error):
     with pytest.raises(expected_error):
-        check_main_diagonal_match(game_board_list)
+        check_main_diagonal_match(game_board_list, width_of_board)
 
  
--- tests/test_check_vertical_match.py
+++ tests/test_check_vertical_match.py
@@ -5,23 +5,23 @@ import pytest
 
 
 @pytest.mark.parametrize(
-  "game_board_list, expected_result",
+  "game_board_list, width_of_board, expected_result",
   [
-    ([[NAME_EMPTY, NAME_EMPTY, NAME_EMPTY],[NAME_EMPTY, 'gert', NAME_EMPTY]], False),
-    ([[NAME_EMPTY, 'A', NAME_EMPTY],[NAME_EMPTY, 'A', NAME_EMPTY]], True),
-    ([[NAME_EMPTY, 'sfgb fg', 'NAME_EMPTY'],[NAME_EMPTY, 'A', 'NAME_EMPTY']], True),
+    ([[NAME_EMPTY, NAME_EMPTY, NAME_EMPTY],[NAME_EMPTY, 'gert', NAME_EMPTY]], 3, False),
+    ([[NAME_EMPTY, 'A', NAME_EMPTY],[NAME_EMPTY, 'A', NAME_EMPTY]], 3, True),
+    ([[NAME_EMPTY, 'sfgb fg', 'NAME_EMPTY'],[NAME_EMPTY, 'A', 'NAME_EMPTY']], 3, True),
   ]      
 )
-def test__check_vertical_match__is_valid(game_board_list, expected_result):
-    assert check_vertical_match(game_board_list) is expected_result
+def test__check_vertical_match__is_valid(game_board_list, width_of_board, expected_result):
+    assert check_vertical_match(game_board_list, width_of_board) is expected_result
 
 
 @pytest.mark.parametrize(
-  "game_board_list, expected_error",
+  "game_board_list, width_of_board, expected_error",
   [
-    ([NAME_EMPTY, NAME_EMPTY, NAME_EMPTY], TypeError),
+    ([NAME_EMPTY, NAME_EMPTY, NAME_EMPTY], 3, TypeError),
   ]      
 )
-def test__check_vertical_match__errors(game_board_list, expected_error):
+def test__check_vertical_match__errors(game_board_list, width_of_board, expected_error):
     with pytest.raises(expected_error):
-        check_vertical_match(game_board_list)
+        check_vertical_match(game_board_list, width_of_board)
